c9a0c720ae2e723e315f37c60abff25bdecb251a
vetuedenoir <kscordel@paul-f3Br8s2.clusters.42paris.fr>
2023-10-05 19:03:53 +0200

    faire ex02 module 0

---
+++ modul0/ex02/Account.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kscordel <kscordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 18:50:56 by kscordel          #+#    #+#             */
+/*   Updated: 2023/10/05 18:56:10 by kscordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static int	Account::getNbAccounts( void )
+{
+
+}
+
+static int	Account::getTotalAmount( void )
+{
+
+}
+
+static int	Account::getNbDeposits( void )
+{
+
+}
+
+static int	Account::getNbWithdrawals( void )
+{
+
+}
+
+static void	Account::displayAccountsInfos( void )
+{
+
+}
+
+Account::Account( int initial_deposit )
+{
+
+}
+
+Account::~Account( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) //const
+{
+
+}
+
+void	Account::displayStatus( void ) //const
+{
+
+}
