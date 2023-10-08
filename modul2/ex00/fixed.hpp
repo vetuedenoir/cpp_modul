76c7a50ecd14d9712ab13a1e9ddf16a8d758c968
vetuedenoir <killianscordel@gmail.com>
2023-10-08 23:32:07 +0200

    debut modul2

---
+++ modul2/ex00/fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+ 	int			nombre;
+ 	static const int	nbBitsFractionnaire = 8;
+ public:
+ 	Fixed ();
+ 	Fixed (const Fixed &copie);
+ 	void	operator=(const Fixed &model);
+ 	~Fixed ();
+ 	int	getRawBits ( void ) const;
+ 	void	setRawBits (int const raw);
+};
+
+#endif
+
