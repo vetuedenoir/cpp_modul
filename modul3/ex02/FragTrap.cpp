2fab80a88846358986dc09a84d454d14e53a4703
vetuedenoir <kscordel@made-f0Br2s5.clusters.42paris.fr>
2023-10-27 19:45:38 +0200

    finir le modul3

---
+++ modul3/ex02/FragTrap.cpp
@@ -0,0 +1,60 @@
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap () : ClapTrap()
+{
+	hitPoints = 100;
+	energiePoints = 100;
+	attackDamage = 30;
+	std::cout << "FragTrap DEFAULT CONSTUCTOR is born." << std::endl;
+}
+
+
+FragTrap::FragTrap (std::string name) : ClapTrap(name)
+{
+	hitPoints = 100;
+	energiePoints = 100;
+	attackDamage = 30;
+	std::cout << "FragTrap " << name << " is born." << std::endl;
+}
+
+FragTrap::FragTrap (const FragTrap &copie) : ClapTrap(copie)
+{
+	hitPoints = 100;
+	energiePoints = 100;
+	attackDamage = 30;
+	*this = copie;
+	std::cout << "FragTrap copie " << name << " is born." << std::endl;
+}
+
+FragTrap::~FragTrap ()
+{
+	std::cout << "FragTrap " << name << " as died." << std::endl;
+}
+
+void    FragTrap::highFivesGuys ()
+{
+    std::cout << "Hey ! Do a High Fives !" << std::endl;
+}
+
+void	FragTrap::operator= (const FragTrap &model)
+{
+	name = model.name;
+	hitPoints = model.hitPoints;
+	energiePoints = model.energiePoints;
+	attackDamage = model.attackDamage;
+}
+
+void	FragTrap::attack (const std::string& target)
+{
+	if (energiePoints > 0)
+	{
+		std::cout << "FragTrap " << name << " attacks " << target
+			<< ", causing " << attackDamage << " points of damage!"
+			<< std::endl;
+		--energiePoints;
+	}
+	else
+		std::cout << "FragTrap " << name << " as not enough energie to attack "
+			<< target << "!" << std::endl;
+}
