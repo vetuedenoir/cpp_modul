f9377dc735c0177e94562a741166197727b51f5f
vetuedenoir <kscordel@made-f0Ar7s11.clusters.42paris.fr>
2024-01-27 22:08:23 +0100

    ex...

--- modul6/ex00/ScalarConverter.cpp
+++ modul6/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: kscordel <kscordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 19:58:31 by kscordel          #+#    #+#             */
-/*   Updated: 2024/01/20 20:02:06 by kscordel         ###   ########.fr       */
+/*   Updated: 2024/01/27 22:07:56 by kscordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,116 @@ ScalarConverter&	ScalarConverter::operator= (const ScalarConverter &model)
 {
 	(void)model;
 	return *this;
+}
+
+bool	ScalarConverter::isChar(std::string str)
+{
+	
+	if (str.length() == 1 && isprint(str[0]))
+		return true;
+	else
+		return false;
+}
+
+bool	ScalarConverter::isInt(std::string str)
+{
+	int	i = 0;
+
+	for (i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i]))
+			return false;
+	}
+	return true;
+}
+
+bool	ScalarConverter::isFloat(std::string str)
+{
+	int	i = 0;
+	int	np = 0;
+
+	if (str.compare("nanf") || str.compare("-inff") || str.compare("+inff") || str.compare("inff"))
+		return true;
+	for (i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			if (i + 1 == str.length() && str[i] != 'f')
+				return false;
+			else if (str[i] == '.')
+			{
+				if (np == 2 || i == 0 || i + 1 == str.length())
+					return false;
+				np++;
+			}
+			if (np > 1)
+				return false;
+		}
+	}
+	if (str[str.length() - 1 ] != 'f')
+		return false;
+	return true;
+}
+
+bool	ScalarConverter::isDouble(std::string str)
+{
+	int	i;
+	int	np = 0;
+	
+	if (str.compare("nan") || str.compare("-inf") || str.compare("+inf") || str.compare("inf"))
+		return true;
+	for (i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			if (str[i] != '.')
+				return false;
+			else if (str[i] == '.' && (i == 0 || i + 1 == str.length()))
+				return false;
+			else if (str[i] == '.')
+				np++;
+			if (np > 1)
+				return false;
+		}
+	}
+	return true;
+}
+
+char	ScalarConverter::strToChar(std::string str)
+{
+	return (str[0]);	
+}
+
+int		ScalarConverter::strToInt(std::string str)
+{
+	return (atoi(str.c_str()));
+}
+
+float	ScalarConverter::strToFloat(std::string str)
+{
+	double	after = 0.0;
+	std::string	past("");
+	
+	int	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == '.')
+			break ;
+		i++;
+	}
+	past = str.substr(i);
+	after = atoi(past.c_str());
+	while (i > 0)
+	{
+		after /= 10;
+		i--;
+	}
+	after += atoi()；
+	
+}
+
+double	ScalarConverter::strToDouble(std::string str)
+{
+	
 }
--- modul6/ex00/ScalarConverter.hpp
+++ modul6/ex00/ScalarConverter.hpp
@@ -6,13 +6,16 @@
 /*   By: kscordel <kscordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 19:56:57 by kscordel          #+#    #+#             */
-/*   Updated: 2024/01/20 19:56:58 by kscordel         ###   ########.fr       */
+/*   Updated: 2024/01/27 19:40:15 by kscordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
+#include <string>
+#include <iostream>
+#include <cctype>
 
 class ScalarConverter
 {
@@ -21,6 +24,16 @@ class ScalarConverter
 	ScalarConverter&	operator= (const ScalarConverter &model);
 public:
 	~ScalarConverter();
+
+	bool	isChar(std::string str);
+	bool	isInt(std::string str);
+	bool	isFloat(std::string str);
+	bool	isDouble(std::string str);
+
+	char	strToChar(std::string str);
+	int		strToInt(std::string str);
+	float	strToFloat(std::string str);
+	double	strToDouble(std::string str);
 
 };
 
